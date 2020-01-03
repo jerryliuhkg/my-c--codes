fc2f47307f9b27e5320064f96c44a220188ba356
jerryliuhkg <53477481+jerryliuhkg@users.noreply.github.com>
2020-01-03 18:46:25 +0800

    Create M16B5_AVX_Pascal-s_Triangle.c

---
+++ Accepted_Tasks/M16B5_AVX_Pascal-s_Triangle.c
@@ -0,0 +1,74 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("inline")
+#pragma GCC optimize("-fgcse")
+#pragma GCC optimize("-fgcse-lm")
+#pragma GCC optimize("-fipa-sra")
+#pragma GCC optimize("-ftree-pre")
+#pragma GCC optimize("-ftree-vrp")
+#pragma GCC optimize("-fpeephole2")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-fsched-spec")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("-falign-jumps")
+#pragma GCC optimize("-falign-loops")
+#pragma GCC optimize("-falign-labels")
+#pragma GCC optimize("-fdevirtualize")
+#pragma GCC optimize("-fcaller-saves")
+#pragma GCC optimize("-fcrossjumping")
+#pragma GCC optimize("-fthread-jumps")
+#pragma GCC optimize("-funroll-loops")
+#pragma GCC optimize("-fwhole-program")
+#pragma GCC optimize("-freorder-blocks")
+#pragma GCC optimize("-fschedule-insns")
+#pragma GCC optimize("inline-functions")
+#pragma GCC optimize("-ftree-tail-merge")
+#pragma GCC optimize("-fschedule-insns2")
+#pragma GCC optimize("-fstrict-aliasing")
+#pragma GCC optimize("-fstrict-overflow")
+#pragma GCC optimize("-falign-functions")
+#pragma GCC optimize("-fcse-skip-blocks")
+#pragma GCC optimize("-fcse-follow-jumps")
+#pragma GCC optimize("-fsched-interblock")
+#pragma GCC optimize("-fpartial-inlining")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("-freorder-functions")
+#pragma GCC optimize("-findirect-inlining")
+#pragma GCC optimize("-fhoist-adjacent-loads")
+#pragma GCC optimize("-frerun-cse-after-loop")
+#pragma GCC optimize("inline-small-functions")
+#pragma GCC optimize("-finline-small-functions")
+#pragma GCC optimize("-ftree-switch-conversion")
+#pragma GCC optimize("-foptimize-sibling-calls")
+#pragma GCC optimize("-fexpensive-optimizations")
+#pragma GCC optimize("-funsafe-loop-optimizations")
+#pragma GCC optimize("inline-functions-called-once")
+#pragma GCC optimize("-fdelete-null-pointer-checks")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <immintrin.h>
+#include <stdio.h>
+#include <string.h>
+
+int t[100001];
+void ncr(int n, int* result) {
+    int i, j;
+    for (i = 0; i <= n; i++) {
+        result[0] = result[i] = 1;
+        for (j = 1; j < i; j++) {
+            result[j] = (t[j - 1] + t[j]) % 1000000007;
+        }
+        memcpy(t, result, sizeof t);
+    }
+}
+
+#ifndef ONLINE_JUDGE
+int a[100001];
+int main() {
+    int n, i;
+    scanf("%d", &n);
+    ncr(n, a);
+    for (i = 0; i <= n; i++) {
+        printf("%d%c", a[i], i == n ? '\n' : ' ');
+    }
+    return 0;
+}
+#endif
