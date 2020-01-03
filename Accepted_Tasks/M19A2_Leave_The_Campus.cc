b1b7095f72178b377b6ee454a3794d06457d726e
jerryliuhkg <53477481+jerryliuhkg@users.noreply.github.com>
2020-01-03 21:42:54 +0800

    Create M19A2_Leave_The_Campus.cc

---
+++ Accepted_Tasks/M19A2_Leave_The_Campus.cc
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    for(int i=0; i<N; i++) {
+        cout << "D";
+    }
+    for(int i=0; i<=M; i++) {
+        cout << "R";
+    }
+    cout << "U";
+
+    return 0;
+}
