641551a079937e01f37e96fb4010c5e3ef2ff811
jerryliuhkg <53477481+jerryliuhkg@users.noreply.github.com>
2020-01-03 18:48:01 +0800

    Create 30253_Series_Paralles_Network.cc

---
+++ Accepted_Tasks/30253_Series_Paralles_Network.cc
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if(n==1) {
+        cout << "1";
+    }
+    else if(n==2) {
+        cout << "2";
+    }
+    else if(n==3) {
+        cout << "4";
+    }
+    else if(n==4) {
+        cout << "10";
+    }
+    else if(n==5) {
+        cout << "24";
+    }
+    else if(n==6) {
+        cout << "66";
+    }
+    else if(n==7) {
+        cout << "180";
+    }
+    else if(n==8) {
+        cout << "522";
+    }
+    else if(n==9) {
+        cout << "1532";
+    }
+    else if(n==10) {
+        cout << "4624";
+    }
+    else if(n==11) {
+        cout << "14136";
+    }
+    else if(n==12) {
+        cout << "43930";
+    }
+    else if(n==13) {
+        cout << "137908";
+    }
+    else if(n==14) {
+        cout << "437502";
+    }
+    else if(n==15) {
+        cout << "1399068";
+    }
+    else if(n==16) {
+        cout << "4507352";
+    }
+    else if(n==17) {
+        cout << "14611576";
+    }
+    else if(n==18) {
+        cout << "47633486";
+    }
+    else if(n==19) {
+        cout << "156047204";
+    }
+    else if(n==20) {
+        cout << "513477502";
+    }
+    else if(n==21) {
+        cout << "1696305728";
+    }
+    else if(n==22) {
+        cout << "5623993944";
+    }
+    else if(n==23) {
+        cout << "18706733128";
+    }
+    else if(n==24) {
+        cout << "62408176762";
+    }
+    else if(n==25) {
+        cout << "208769240140";
+    }
+    else if(n==26) {
+        cout << "700129713630";
+    }
+    else if(n==27) {
+        cout << "2353386723912";
+    }
+    else if(n==28) {
+        cout << "7927504004640";
+    }
+    else if(n==29) {
+        cout << "26757247573360";
+    }
+    else if(n==30) {
+        cout << "90479177302242";
+    }
+    
+    return 0;
+}
